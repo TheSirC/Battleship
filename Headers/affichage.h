4db3001499e2e680f19aa9319a32ef2a06703b8d
TheSirC <clocloranely@hotmail.fr>
2016-05-29 20:21:26 +0200

    Quatrième version d'affichage
    Cette dernière version est totalement fonctionnelle et intégrable au
    projet. Elle interagit aux clics et renvoie des données sous formes de
    couplets (x,y) qui sont normalisés pour le traitement : (0,0) pour en
    haut à gauche, (10,10) pour en bas à droite.
    Le bug de double affichage et celui de la création d'affichage en boucle
    infini ont été résolus.

--- Headers/affichage.h
+++ Headers/affichage.h
@@ -10,13 +10,16 @@ using namespace cimg_library;
 class affichage
 {
 public:
-	affichage(unsigned char couleurGrille[3], int nombreLignes = 10, int nombreColonnes = 10); // Crée une grille avec les paramètres donnés par défaut ou ceux rentrés par l'utilisateur
+	affichage(unsigned char couleurGrille[3], int nombreLignes_ = 10, int nombreColonnes_ = 10); // Crée une grille avec les paramètres donnés par défaut ou ceux rentrés par l'utilisateur
+	void setNombreLignes(int nb) { nombreLignes_ = nb; }
+	void setNombreColonnes(int nb) { nombreColonnes_ = nb; }
+	int getNombreLignes() { return nombreLignes_; }
+	int getNombreColonnes() { return nombreColonnes_; }
 
 private:
-	int nombreLignes, nombreColonnes;
+	int nombreLignes_, nombreColonnes_;
 	unsigned char couleurGrille[3], couleurJoueur1[3], couleurJoueur2[3];
-	void actualiseGrille(CImgDisplay disp);
-	vector<int> clic(int nombreColonnes, int nombreLignes, CImgDisplay disp);
+	vector<int> coordonnesClic; // Vecteur contenant les coordonnes des clics
 };
 
 #endif __AFFICHAGE__
--- Sources/test_affichage.cpp
+++ Sources/test_affichage.cpp
@@ -3,7 +3,6 @@
 int main() {
 
 	unsigned char couleurGrille[3] = { 0,0,255 };
-	affichage Grille(couleurGrille,10,10); //Test simple de la grille d'affichage
-	
+	affichage Grille(couleurGrille,10,10); // Test simple de la grille d'affichage
 	return 0;
 }
