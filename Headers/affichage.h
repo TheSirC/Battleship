a53fa7e761a8bdf0e3faabe99bcd73053bef2b91
TheSirC <clocloranely@hotmail.fr>
2016-06-02 22:21:18 +0200

    Affinage de la classe affichage
    Ajout du vecteur contenant les deux derniers clics de l'utilisateur dans
    le champ public de la classe pour que les autres méthodes puissent y
    accéder.
    Gestion de la mémoire concernant la taille du vecteur : à chaque ajout
    le vecteur est réinitialisé.

--- Headers/affichage.h
+++ Headers/affichage.h
@@ -10,16 +10,16 @@ using namespace cimg_library;
 class affichage
 {
 public:
-	affichage(unsigned char couleurGrille[3], int nombreLignes_ = 10, int nombreColonnes_ = 10); // Crée une grille avec les paramètres donnés par défaut ou ceux rentrés par l'utilisateur
-	void setNombreLignes(int nb) { nombreLignes_ = nb; }
-	void setNombreColonnes(int nb) { nombreColonnes_ = nb; }
-	int getNombreLignes() { return nombreLignes_; }
-	int getNombreColonnes() { return nombreColonnes_; }
+		affichage(unsigned char couleurGrille[3], int nombreLignes_ = 10, int nombreColonnes_ = 10); // Crée une grille avec les paramètres donnés par défaut ou ceux rentrés par l'utilisateur
+		void setNombreLignes(int nb) { nombreLignes_ = nb; }
+		void setNombreColonnes(int nb) { nombreColonnes_ = nb; }
+		int getNombreLignes() { return nombreLignes_; }
+		int getNombreColonnes() { return nombreColonnes_; }
+		vector<int> coordonnesClic; // Vecteur contenant les coordonnées des clics
 
 private:
-	int nombreLignes_, nombreColonnes_;
-	unsigned char couleurGrille[3], couleurJoueur1[3], couleurJoueur2[3];
-	vector<int> coordonnesClic; // Vecteur contenant les coordonnes des clics
+		int nombreLignes_, nombreColonnes_;
+		unsigned char couleurGrille[3], couleurJoueur1[3], couleurJoueur2[3];
 };
 
 #endif __AFFICHAGE__
--- Sources/affichage.cpp
+++ Sources/affichage.cpp
@@ -2,29 +2,30 @@
 
 affichage::affichage(unsigned char couleurGrille[3], int nombreLignes_, int nombreColonnes_)
 {
-  // Construit une grille paramétrée qui donne les coordoonées pour tous les autres points
-  CImg<unsigned char> grid(CImgDisplay::screen_height()/2,CImgDisplay::screen_height()/2, 1, 3, 255);
-  // Tracage des lignes et des colonnes
-	int tailleEcran = grid.height(); // Pré-allocation du résultat de la fonction pour optimisation
-	for (int i = 1; i < nombreLignes_; i++) grid.draw_line(0, (tailleEcran / nombreLignes_ )* i, tailleEcran, (tailleEcran / nombreLignes_) * i, couleurGrille); // Trace les lignes horizontales
-	for (int i = 1; i < nombreColonnes_; i++) grid.draw_line((tailleEcran / nombreColonnes_)* i, 0, (tailleEcran / nombreColonnes_) * i, tailleEcran, couleurGrille); // Trace les lignes verticales 
-	CImgDisplay disp(grid, "Bataille navale", 0, false, false); // Crée la fenêtre associée à la grille
-	disp.move((CImgDisplay::screen_width() - tailleEcran) / 2, (CImgDisplay::screen_height() - disp.height()) / 2); // Dépalce la fenêtre au centre de l'écran
-	CImg<unsigned char> scene = grid;
-	// Attend qu'un évènement se produise sur la fenêtre sélectionnée
-	disp.wait();
-	while (!disp.is_closed() && !disp.is_keyESC() && !disp.is_keyQ()) {
-			scene.display(disp);
-			// Détection des clics gauche de souris
-			if (disp.button() & 1) {
-					int cX = nombreLignes_*disp.mouse_x() / disp.width();
-					int cY = nombreColonnes_*disp.mouse_y() / disp.height();
-					coordonnesClic.push_back(cX);
-					coordonnesClic.push_back(cY);
-			}
-			// Permet de gérer le redimmensionnement de la fenêtre
-			if (disp.is_resized()) disp.resize();
-			// Attend qu'un évènement se produise sur la fenêtre sélectionnée
-			disp.wait();
-	}
+		// Construit une grille paramétrée qui donne les coordoonées pour tous les autres points
+		CImg<unsigned char> grid(CImgDisplay::screen_height() / 2, CImgDisplay::screen_height() / 2, 1, 3, 255);
+		// Tracage des lignes et des colonnes
+		int tailleEcran = grid.height(); // Pré-allocation du résultat de la fonction pour optimisation
+		for (int i = 1; i < nombreLignes_; i++) grid.draw_line(0, (tailleEcran / nombreLignes_)* i, tailleEcran, (tailleEcran / nombreLignes_) * i, couleurGrille); // Trace les lignes horizontales
+		for (int i = 1; i < nombreColonnes_; i++) grid.draw_line((tailleEcran / nombreColonnes_)* i, 0, (tailleEcran / nombreColonnes_) * i, tailleEcran, couleurGrille); // Trace les lignes verticales 
+		CImgDisplay disp(grid, "Bataille navale", 0, false, false); // Crée la fenêtre associée à la grille
+		disp.move((CImgDisplay::screen_width() - tailleEcran) / 2, (CImgDisplay::screen_height() - disp.height()) / 2); // Dépalce la fenêtre au centre de l'écran
+		CImg<unsigned char> scene = grid;
+		// Attend qu'un évènement se produise sur la fenêtre sélectionnée
+		disp.wait();
+		while (!disp.is_closed() && !disp.is_keyESC() && !disp.is_keyQ()) {
+				scene.display(disp);
+				// Détection des clics gauche de souris
+				if (disp.button() & 1) {
+						int cX = nombreLignes_*disp.mouse_x() / disp.width();
+						int cY = nombreColonnes_*disp.mouse_y() / disp.height();
+						coordonnesClic.clear(); // Réinitialisation du vecteur pour ne pas avoir une croissance de sa taille		
+						coordonnesClic.push_back(cX);
+						coordonnesClic.push_back(cY);
+				}
+				// Permet de gérer le redimmensionnement de la fenêtre
+				if (disp.is_resized()) disp.resize();
+				// Attend qu'un évènement se produise sur la fenêtre sélectionnée
+				disp.wait();
+		}
 }
