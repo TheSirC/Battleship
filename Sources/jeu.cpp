f7b2bc5f6ccd3519a762835b2b4fc4e4c0a67e53
TheSirC <clocloranely@hotmail.fr>
2016-06-03 15:22:30 +0200

    Deuxième version de la classe Jeu
    Ajout de l'initialisation de la grille logique
    Amélioration de la procédure du placement des bateaux des joueurs humain et artificiels
    Ajout des procédures codant pour les tours de l'IA

--- Sources/jeu.cpp
+++ Sources/jeu.cpp
@@ -6,7 +6,10 @@ Jeu::Jeu(string NomJoueur, int nombreBateau_)
 		unsigned char couleurGrille[3] = { 0,0,255 };
 		affichage Grille(couleurGrille, 10, 10);
 
-		//Initilisation des joueurs (humain et IA)
+		// Initilaisation de la grille logique
+		grilleLogique grilleLogique();
+
+		// Initilisation des joueurs (humain et IA)
 		JoueurHumain* Joueur1 = new JoueurHumain;
 		JoueurOrdi* Joueur2 = new JoueurOrdi;
 
@@ -16,31 +19,86 @@ Jeu::Jeu(string NomJoueur, int nombreBateau_)
 		{
 				cout << " Placez la proue du bateau." << endl;
 				int clicX1 = Grille.coordonnesClic[0], clicY1 = Grille.coordonnesClic[1];
-				listeBateau[i].positionX = clicX1;
-				listeBateau[i].positionY = clicY1;
+				Joueur1->listeBateau[i].positionX = clicX1;
+				Joueur1->listeBateau[i].positionY = clicY1;
 				cout << "Selectionnez l'orientation du bateau." << endl;
 				int clicX2 = Grille.coordonnesClic[0], clicY2 = Grille.coordonnesClic[1];
-				if (clicX1 > clicX2)
+				if (clicX1 >= clicX2)
 				{
-						listeBateau[i].orientation = 3;
+						Joueur1->listeBateau[i].orientation = 3;
 				}
-				else if (clicY1>clicY2)
+				else if (clicY1 >= clicY2)
 				{
-						listeBateau[i].orientation = 2;
+						Joueur1->listeBateau[i].orientation = 2;
 				}
 				else if (clicY1<clicY2)
 				{
-						listeBateau[i].orientation = 0;
+						Joueur1->listeBateau[i].orientation = 0;
 				}
 				else
 				{
-						listeBateau[i].orientation = 1;
+						Joueur1->listeBateau[i].orientation = 1;
 				}
 		}
 
 		// Placement des bateaux de l'IA
 		cout << "Placement des bateaux par votre adversaire." << endl;
-		Joueur2.choixCibleOrdi();
+		bool aReussiATirer1 = 0;
+		bool aReussiATirer2 = 0;
+		for (size_t i = 0; i < Joueur2->listeBateau.size(); i++)
+		{
+			while (!aReussiATirer1)
+			{
+				Joueur2.choixCibleOrdi(Grille.nombreLignes, Grille.nombreColonnes);
+				int cOX1 = Joueur2.getCibleX(); // Première coordonnee X choisie par l'ordinateur
+				int cOY1 = Joueur2.getCibleY(); // Première coordonnee Y choisie par l'ordinateur
+				switch (grilleLogique.etatCase(cOX1, cOY1))
+				{
+				case 1:
+					aReussiATirer1 = !aReussiATirer1;
+					break;
+				case 3:
+					aReussiATirer1 = !aReussiATirer1;
+					break;
+				default:
+					break;
+				}
+			}
+
+			while (!aReussiATirer2)
+			{
+				Joueur2.choixCibleOrdi(Grille.nombreLignes, Grille.nombreColonnes);
+				int cOX2 = Joueur2.getCibleX(); // Deuxième coordonnee X choisie par l'ordinateur
+				int cOY2 = Joueur2.getCibleY(); // Deuxième coordonnee Y choisie par l'ordinateur
+				switch (grilleLogique.etatCase(cOX, cOY))
+				{
+					case 1:
+						aReussiATirer2 = !aReussiATirer2;
+						break;
+					case 3:
+						aReussiATirer2 = !aReussiATirer2;
+						break;
+					default:
+						break;
+				}
+				if (cOX1 >= cOX2)
+				{
+					Joueur2->listeBateau[i].orientation = 3;
+				}
+				else if (cOY1 >= cOY2)
+				{
+					Joueur2->listeBateau[i].orientation = 2;
+				}
+				else if (cOY1<cOY2)
+				{
+					Joueur2->listeBateau[i].orientation = 0;
+				}
+				else
+				{
+					Joueur2->listeBateau[i].orientation = 1;
+				}
+			}
+		
 
 		// Déroulement du jeu
 		while (!partieTerminee)
@@ -50,7 +108,31 @@ Jeu::Jeu(string NomJoueur, int nombreBateau_)
 						cout << "C'est votre tour !" << endl;
 						int clicX1 = Grille.coordonnesClic[0], clicY1 = Grille.coordonnesClic[1];
 						if ((clicX1<0) || (clicX1>Grille.getNombreLignes()) || (clicY1<0) || (clicY1>Grille.getNombreColonnes()))
-								continue;
+								
+						quelJoueur_ = !quelJoueur_;
+				}
+				else
+				{
+					// Tour de l'IA
+					cout << "C'est au tour de votre adversaire." << endl;
+					bool aReussiATirer = 0;
+					while (!aReussiATirer)
+					{
+						Joueur2.choixCibleOrdi(Grille.nombreLignes, Grille.nombreColonnes);
+						int cOX = Joueur2.getCibleX(); // Coordonnee X choisie par l'ordinateur
+						int cOY = Joueur2.getCibleY(); // Coordonnee Y choisie par l'ordinateur
+						switch (grilleLogique.etatCase(cOX, cOY)):
+							case 1:
+								aReussiATirer = !aReussiATirer;
+								break;
+							case 3:
+								aReussiATirer = !aReussiATirer;
+								break;
+							default:
+								break;
+					}	
+
+					quelJoueur_ = !quelJoueur_;
 				}
 				
 		}
