8feb225a4daf39a931a73120d4e9279beb23e861
DonJons42 <adsquall@gmail.com>
2016-06-06 10:32:34 +0200

    Changements Ok

--- Sources/JoueurHumain.cpp
+++ Sources/JoueurHumain.cpp
@@ -13,27 +13,26 @@ void JoueurHumain::ChoixCible(int CibleX, int CibleY)
 }
 
 
-void JoueurHumain::ajouterBateau(int cX1, int cY1, int cX2, int cY2, Personne Joueur, int i)
+void JoueurHumain::ajouterBateau(int cX1, int cY1, int cX2, int cY2, int i)
 {
-	int clicX1 = Grille.coordonnesClic[0], clicY1 = Grille.coordonnesClic[1];// faire correspondre
-	Joueur.flotte_[i].setPositionX(clicX1);
-	Joueur.flotte_[i].setPositionY(clicY1);
-	cout << "Selectionnez l'orientation du bateau." << endl;
-	int clicX2 = Grille.coordonnesClic[0], clicY2 = Grille.coordonnesClic[1];
+	int clicX1 = cX1, clicY1 = cY1;// faire correspondre
+	getFlotte()->at(i).setPositionX(clicX1);
+	getFlotte()->at(i).setPositionY(clicY1);
+	int clicX2 = cX2, clicY2 = cY2;
 	if (clicX1 >= clicX2)
 	{
-		Joueur.flotte_[i].setOrientation(3);
+		getFlotte()->at(i).setOrientation(3);
 	}
 	else if (clicY1 >= clicY2)
 	{
-		Joueur.flotte_[i].setOrientation(2);
+		getFlotte()->at(i).setOrientation(2);
 	}
 	else if (clicY1 < clicY2)
 	{
-		Joueur.flotte_[i].setOrientation(0);
+		getFlotte()->at(i).setOrientation(0);
 	}
 	else
 	{
-		Joueur.flotte_[i].setOrientation(1);
+		getFlotte()->at(i).setOrientation(1);
 	}
 }
--- Sources/JoueurOrdi.cpp
+++ Sources/JoueurOrdi.cpp
@@ -1,39 +1,35 @@
 #include "../Headers/JoueurOrdi.h"
 #include <stdlib.h> // pour la fonction rand()
 
-JoueurOrdi::JoueurOrdi() : Personne(const string &nom_, int NbBateaux_)
-{
-
-}
 
 void JoueurOrdi::ChoixCible(int nbLignes, int nbColonnes)
 {
-	CibleX_ = rand() % (nbColonnes) ; //le +1 pour partir de la colonne 1
-	CibleY_ = rand() % (nbLignes) ;
+	CibleX_ = rand() % (nbColonnes) ; // Fonction qui choisie une colonne aléatoirement (coordonée X)
+	CibleY_ = rand() % (nbLignes) ; // Fonction qui choisie une lugne aléatoirement (coordonée Y)
 }
 
-void JoueurOrdi::ajouterBateau(int nbColonnes, int nbLignes, Personne Ordi, int i)
+void JoueurOrdi::ajouterBateau(int nbColonnes, int nbLignes, int i)//
 {
 	int clicX1 = rand() % (nbColonnes);
 	int clicY1 = rand() % (nbLignes);
-	Ordi.flotte_[i].setPositionX(clicX1);
-	Ordi.flotte_[i].setPositionY(clicY1);
+	getFlotte()->at(i).setPositionX(clicX1);
+	getFlotte()->at(i).setPositionY(clicY1);
 	int clicX2 = rand() % (nbColonnes);
 	int clicY2 = rand() % (nbLignes);
 	if (clicX1 >= clicX2)
 	{
-		Ordi.flotte_[i].setOrientation(3);
+		getFlotte()->at(i).setOrientation(3);
 	}
 	else if (clicY1 >= clicY2)
 	{
-		Ordi.flotte_[i].setOrientation(2);
+		getFlotte()->at(i).setOrientation(2);
 	}
 	else if (clicY1 < clicY2)
 	{
-		Ordi.flotte_[i].setOrientation(0);
+		getFlotte()->at(i).setOrientation(0);
 	}
 	else
 	{
-		Ordi.flotte_[i].setOrientation(1);
+		getFlotte()->at(i).setOrientation(1);
 	}
 }
