ab5120f7178288b00e4ff4d1e6bda22ebd0ee14f
DonJons42 <adsquall@gmail.com>
2016-06-06 10:30:24 +0200

    Marche

--- Headers/GrilleLogique.h
+++ Headers/GrilleLogique.h
@@ -1,29 +1,22 @@
 #include <vector>
+#include <string>
 #include <iostream>
+#include "../Headers/Bateau.h"
 
 using namespace std;
 
-#ifndef _GRILLELOGIQUE_
-#define _GRILLELOGIQUE_
-
-class GrilleLogique
+class GrilleLogique /// modèle diapo 129 type template => tableaux (séance 5)
 {
-		int nombreLignes_ = 10, nombreColonnes_ = 10;
-		int** Grille_; // Pointeur vers la grille 2D
+	int nbLignes_;
+	int nbColonnes_;
+	vector <Bateau> *Flotte; // contient les positions des bateaux
 
 public:
-		GrilleLogique(int nombreLignes_ = 10, int nombreColonnes_= 10);
-		~GrilleLogique();
+	GrilleLogique(int nbLignes_ = 10, int nbColonnes_ = 10); // constructeur
+	~GrilleLogique();// destructeur
+	void SetPositionBateaux(); // renvoie la position des bateaux (sélectionnés)
+	void GetPositionBateaux(); // entre la  position d'un bateau
+	void AfficherParametres(); //renvoie le nb de colonnes et de cases
 
 
-		//int getEtatCase(int x, int y); // Retourne l'état de la case donnée en paramètre : 1 -> Mer, 2 -> Mer touchée, 3 -> Bateau découvert, 4 -> Bateau touché 
-		//void setEtatCase(int x, int y, int Etat); // Définis l'état de la case donnée en paramètre : 1 -> Mer, 2 -> Mer touchée, 3 -> Bateau découvert, 4 -> Bateau touché 
-		//bool tireSurCase(int x, int y); // Renvoi de la présence d'un bateau ou non
-
-		void afficherGrille(); 
 };
-
-
-
-#endif // !_GRILLELOGIQUE_
-
---
+++ Headers/Personne.h
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "../Headers/Bateau.h"
+
+using namespace std;
+
+class Personne
+{
+protected:
+	string nom_;
+	int nbBateaux_;
+	vector<Bateau> flotte_;
+public:
+
+	Personne(const string &nom_="", int NbBateaux_ = 6);
+	void setNom(string nom) { nom_ = nom; }
+	void setNbBateaux(int n) { nbBateaux_ = n; }
+	string getNom() { return nom_; }
+	int getNbBateaux() {return nbBateaux_; }
+	vector<Bateau>* getFlotte() { return &flotte_; }
+
+	virtual void ajouterBateau();
+	virtual void ChoixCible(); //on récupère la méthode choixCible soit de JoueurHumain, soit de JoueurOrdi
+};
