2d99a99224736c10132c5c54c426884fdd1bd0d2
TheSirC <clocloranely@hotmail.fr>
2016-06-06 10:41:20 +0200

    Ajout des sources pour la classe GrilleLogique
    Le source de la classe comprend les définitions des fonctions :
    - de déclaration et d'initialisation des cases de la grille
    - d'affichage des états des cases de la grille
    - de vérifier l'état de la grille (touché ou pas)
    
    Le source de test teste toutes les fonctions précédentes ("Duh !")

--- Sources/GrilleLogique.cpp
+++ Sources/GrilleLogique.cpp
@@ -3,7 +3,7 @@
 GrilleLogique::GrilleLogique(int nombreLignes_, int nombreColonnes_)
 {
 		// Déclaration de la grille
-		int** Grille_ = new int*[nombreLignes_]; // Déclaration des lignes de la grille 2D
+		this->Grille_ = new int*[nombreLignes_]; // Déclaration des lignes de la grille 2D
 		for (int i = 0; i < nombreLignes_; i++)
 		{
 				Grille_[i] = new int[nombreColonnes_]; // Déclaration des colonnes de la grille 2D
@@ -30,57 +30,56 @@ GrilleLogique::~GrilleLogique()
 		delete[] Grille_; // Destruction du pointeur vers le tableau
 }
 
-//GrilleLogique::getEtatCase(int x, int y)
-//{
-//
-//}
-//
-//GrilleLogique::setEtatCase(int x, int y, int Etat)
-//{
-//
-//}
-//
-//GrilleLogique::tireSurCase(int x, int y)
-//{
-//
-//}
+int GrilleLogique::getEtatCase(int x, int y)
+{
+	return Grille_[x][y];
+}
+
+void GrilleLogique::setEtatCase(int x, int y, int Etat)
+{
+	Grille_[x][y] = Etat;
+}
+
+bool GrilleLogique::tireSurCase(int x, int y)
+{
+	if (Grille_[x][y] == 1)
+	{
+		cout << "Dommage, c'est de l'eau." << endl;
+		return 1;
+	}
+	else if (Grille_[x][y] == 3)
+	{
+		cout << "Bien joué ! C'était un bateau !" << endl;
+		return 1;
+	}
+	else
+	{
+		cout << "Vous avez deja bombarde cette case !" << endl;
+		return 0;
+	}
+}
 
 void GrilleLogique::afficherGrille()
 {
-		for (int i = 0; i < nombreColonnes_; i++)
-		{
-				cout << " " << i << " " << endl;
-		}
 
 		for (int i = 0; i < nombreLignes_; i++)
 		{
-				
 				for (int j = 0; j < nombreColonnes_; j++)
 				{
-						cout << "____" << endl << i << " |";
-				}
-
-				for (int j = 0; j < nombreColonnes_; j++)
-				{
-						if ((Grille_[i][j] == 2) && (Grille_[i][j] == 4)) // Vérifie la condition concernant le fait que la case est déjà été testée
+					/*cout << " " << Grille_[i][j] << " ";*/
+						if ((Grille_[i][j] == 2) || (Grille_[i][j] == 4)) // Vérifie la condition concernant le fait que la case est déjà été testée
 						{
 								cout << " X ";
 						}
 						else if (Grille_[i][j] == 1)
 						{
-								cout << "  ";
+								cout << " M ";
 						}
 						else
 						{
 								cout << " B ";
 						}
-						cout << "|";
 				}
 				cout << endl;
-		}
-
-		for (int j = 0; j < nombreColonnes_; j++)
-		{
-				cout << "____" << endl;
 		}
 }
